f787e85c79904ee01688f59aa74a647f96b65de0
LamLe25 <letruonglam2511@gmail.com>
2021-07-14 09:58:45 +0700

    Add Mul Func

--- Math.cpp
+++ Math.cpp
@@ -11,8 +11,14 @@ int Sub(const int& a, const int& b)
 	return a - b;
 }
 
+int Mul(const int& a, const int& b)
+{
+	return a * b;
+}
+
 int main()
 {
 	cout << "Sum : " << Sum(1, 2)<<endl;
 	cout << "Sub: " << Sub(2, 1) << endl;
+	cout << "Mul: " << Mul(2, 3) << endl;
 }
