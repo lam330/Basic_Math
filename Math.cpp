7d96b23924bf5551474a236d1fe674453c566b5d
LamLe25 <letruonglam2511@gmail.com>
2021-07-14 21:52:45 +0700

    Add Div Func

--- Math.cpp
+++ Math.cpp
@@ -16,9 +16,17 @@ int Mul(const int& a, const int& b)
 	return a * b;
 }
 
+int Div(const int& a, const int& b)
+{
+	return a / b;
+}
+
+
+
 int main()
 {
 	cout << "Sum : " << Sum(1, 2)<<endl;
 	cout << "Sub: " << Sub(2, 1) << endl;
 	cout << "Mul: " << Mul(2, 3) << endl;
+	cout << "Div: " << Div(2, 1) << endl;
 }
