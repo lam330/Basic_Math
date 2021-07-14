e881605c7c47ed17d66690a3f5ae1ce658e02b3c
LamLe25 <letruonglam2511@gmail.com>
2021-07-14 09:11:24 +0700

    Add Sub Func

--- Math.cpp
+++ Math.cpp
@@ -6,7 +6,13 @@ int Sum(const int& a, const int& b)
 	return a + b;
 }
 
+int Sub(const int& a, const int& b)
+{
+	return a - b;
+}
+
 int main()
 {
 	cout << "Sum : " << Sum(1, 2)<<endl;
+	cout << "Sub: " << Sub(2, 1) << endl;
 }
